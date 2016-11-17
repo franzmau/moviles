49d1bd1eb4915e4ad11a02d536c9698da1e2185b
Francisco Mauricio Villanueva Martínez <franzmau@hotmail.com>
2016-11-16 23:35:01 -0600

    Problems with views

---
+++ Prestamos/app/src/main/java/com/example/marcolopez/prestamos/Deudas.java
@@ -0,0 +1,105 @@
+package com.example.marcolopez.prestamos;
+
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.List;
+
+public class Deudas extends AppCompatActivity {
+
+    ListView deudas;
+    ListView prestamos;
+    TextView logoutTxt;
+    Integer [] debess={
+            R.drawable.debes
+    };
+    Integer [] prestamo={
+            R.drawable.teben
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deudas);
+
+        deudas = (ListView) findViewById(R.id.debes);
+        prestamos=(ListView)findViewById(R.id.te_deben);
+        logoutTxt = (TextView) findViewById(R.id.logoutTxt);
+
+        cargardeudas();
+        cargarprestamos();
+
+        logoutTxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Inicio de sesión
+                ParseUser.logOut();
+                Intent i = new Intent(Deudas.this, MainActivity.class);
+                startActivity(i);
+                finish();
+                return;
+            }
+        });
+    }
+    public void cargarprestamos(){
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("prestamos");
+
+        query.whereEqualTo("lender", ParseUser.getCurrentUser().getUsername());
+        query.findInBackground(new FindCallback<ParseObject>() {
+            public void done(List<ParseObject> list, ParseException e) {
+                if (e == null) {
+
+                    final ParseObject[] objects = list.toArray(new ParseObject[list.size()]);
+                    ParseObject[] dataList = new ParseObject[list.size()];
+                    for (int i = 0; i < list.size(); i++) {
+                        dataList[i] = (ParseObject) list.get(i);
+                    }
+                    DeudasAdapter adapter = new DeudasAdapter(Deudas.this,
+                            R.layout.list_prestamos, objects, prestamo[0]);
+                    prestamos.setDivider(new ColorDrawable(0xFFFFFF));
+                    prestamos.setDividerHeight(1);
+                    prestamos.setAdapter(adapter);
+
+                }else{
+                }
+            }
+        });
+    }
+
+    public void cargardeudas(){
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("prestamos");
+
+        query.whereEqualTo("borrower", ParseUser.getCurrentUser().getUsername());
+        query.findInBackground(new FindCallback<ParseObject>() {
+            public void done(List<ParseObject> list, ParseException e) {
+                if (e == null) {
+
+                    final ParseObject[] objects = list.toArray(new ParseObject[list.size()]);
+                    ParseObject[] dataList = new ParseObject[list.size()];
+                    for (int i = 0; i < list.size(); i++) {
+                        dataList[i] = (ParseObject) list.get(i);
+                    }
+                    DeudasAdapter adapter = new DeudasAdapter(Deudas.this,
+                            R.layout.list_deudas, objects, debess[0]);
+                    deudas.setDivider(new ColorDrawable(0xFFFFFF));
+                    deudas.setDividerHeight(1);
+                    deudas.setAdapter(adapter);
+
+                }else{
+                }
+            }
+        });
+    }
+
+
+}
---
+++ Prestamos/app/src/main/java/com/example/marcolopez/prestamos/DeudasAdapter.java
@@ -0,0 +1,58 @@
+package com.example.marcolopez.prestamos;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.parse.ParseObject;
+
+
+
+public class DeudasAdapter extends ArrayAdapter<ParseObject> {
+    private final Activity context;
+    private ParseObject[] data;
+    int layoutResourceId;
+    private final Integer imageId;
+
+
+    public DeudasAdapter(Activity context, int layoutResourceId, ParseObject[] data, Integer imageId) {
+        super(context, layoutResourceId, data);
+        this.context = context;
+        this.data = data;
+        this.layoutResourceId = layoutResourceId;
+        this.imageId = imageId;
+    }
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        View row = convertView;
+        Holder holder = null;
+        if (row == null) {
+            LayoutInflater inflater = ((Activity) context).getLayoutInflater();
+            row = inflater.inflate(layoutResourceId, parent, false);
+            holder = new Holder();
+            holder.nombrede= (TextView) row.findViewById(R.id.nombreAmigoTxtd);
+            holder.cantidad=(TextView) row.findViewById(R.id.cantidadd);
+            holder.tiempo=(TextView) row.findViewById(R.id.tiempod);
+            row.setTag(holder);
+        } else {
+            holder = (Holder) row.getTag();
+        }
+        holder.nombrede.setText(data[position].getString("lender"));
+        holder.cantidad.setText(data[position].getString("deuda"));
+        ImageView imageView=(ImageView) row.findViewById(R.id.imgd);
+        imageView.setImageResource(imageId);
+        return row;
+    }
+    static class Holder {
+        TextView nombrede;
+        TextView cantidad;
+        TextView tiempo;
+
+
+
+    }
+}
