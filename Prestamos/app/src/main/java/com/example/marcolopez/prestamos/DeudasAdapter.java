7aa92f1c3a744841acf6bd6f43ae62ed66a084e6
Francisco Mauricio Villanueva Martínez <franzmau@hotmail.com>
2016-11-28 17:48:05 -0600

    fix view

--- Prestamos/app/src/main/java/com/example/marcolopez/prestamos/DeudasAdapter.java
+++ Prestamos/app/src/main/java/com/example/marcolopez/prestamos/DeudasAdapter.java
@@ -8,6 +8,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.parse.Parse;
 import com.parse.ParseObject;
 
 
@@ -17,41 +18,78 @@ public class DeudasAdapter extends ArrayAdapter<ParseObject> {
     private ParseObject[] data;
     int layoutResourceId;
     private final Integer imageId;
+    public int i=0;
 
 
-    public DeudasAdapter(Activity context, int layoutResourceId, ParseObject[] data, Integer imageId) {
+
+    public DeudasAdapter(Activity context, int layoutResourceId, ParseObject[] data, Integer imageId, int i) {
         super(context, layoutResourceId, data);
         this.context = context;
         this.data = data;
         this.layoutResourceId = layoutResourceId;
         this.imageId = imageId;
+        this.i=i;
+        for (int ih=0;ih<data.length;ih++){
+            System.out.println(data[ih].getDouble("deuda")+" aqui estan los datos");
+        }
     }
+
     @Override
     public View getView(final int position, View convertView, ViewGroup parent) {
         View row = convertView;
         Holder holder = null;
         if (row == null) {
-            LayoutInflater inflater = ((Activity) context).getLayoutInflater();
-            row = inflater.inflate(layoutResourceId, parent, false);
-            holder = new Holder();
-            holder.nombrede= (TextView) row.findViewById(R.id.nombreAmigoTxtd);
-            holder.cantidad=(TextView) row.findViewById(R.id.cantidadd);
-            holder.tiempo=(TextView) row.findViewById(R.id.tiempod);
-            row.setTag(holder);
+                if(get_i()==0) {
+                    LayoutInflater inflater = ((Activity) context).getLayoutInflater();
+                    row = inflater.inflate(layoutResourceId, parent, false);
+                    holder = new Holder();
+                    holder.nombrede = (TextView) row.findViewById(R.id.nombreAmigoTxtd);
+                    holder.cantidad = (TextView) row.findViewById(R.id.cantidadd);
+                    holder.tiempo = (TextView) row.findViewById(R.id.tiempod);
+                    row.setTag(holder);
+                }else{
+                    LayoutInflater inflater = ((Activity) context).getLayoutInflater();
+                    row = inflater.inflate(layoutResourceId, parent, false);
+                    holder = new Holder();
+                    holder.nombrep = (TextView) row.findViewById(R.id.nombreAmigoTxtp);
+                    holder.cantidap = (TextView) row.findViewById(R.id.cantidadp);
+                    holder.tiempop = (TextView) row.findViewById(R.id.tiempop);
+                    row.setTag(holder);
+                }
+
         } else {
             holder = (Holder) row.getTag();
         }
-        holder.nombrede.setText(data[position].getString("lender"));
-        holder.cantidad.setText(data[position].getString("deuda"));
-        ImageView imageView=(ImageView) row.findViewById(R.id.imgd);
-        imageView.setImageResource(imageId);
+
+           if(get_i()==0) {
+               holder.nombrede.setText(data[position].getString("lender"));
+               holder.cantidad.setText(data[position].getInt("deuda"));
+               ImageView imageView = (ImageView) row.findViewById(R.id.imgd);
+               imageView.setImageResource(imageId);
+           }else {
+               holder.nombrep.setText(data[position].getString("borrower"));
+                String a=String.valueOf(data[position].getInt("deuda"));
+
+               holder.cantidap.setText(a);
+               ImageView imageView = (ImageView) row.findViewById(R.id.imgp);
+               imageView.setImageResource(imageId);
+           }
         return row;
     }
+
+
+    public int get_i(){
+        return this.i;
+    }
+
     static class Holder {
         TextView nombrede;
         TextView cantidad;
         TextView tiempo;
 
+        TextView nombrep;
+        TextView cantidap;
+        TextView tiempop;
 
 
     }
